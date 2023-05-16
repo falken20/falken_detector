c78f1e0d76071b03623f47780a3c5ebdeaec642f
Richi <r.rodriguez@bbva.com>
2023-05-16 08:29:02 +0200

    Adding detect image method

--- falken_detector/main.py
+++ falken_detector/main.py
@@ -1,9 +1,12 @@
 # by Richi Rod AKA @richionline / falken20
 # ./falken_detector/main.py
 
+import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+import cv2
+
 
 # The mediapipe object detector requires a trained model
 # More information and trained models:
@@ -11,10 +14,49 @@ from mediapipe.tasks.python import vision
 model_path = './trained_model/efficientdet_lite0.tflite'
 
 
-BaseOptions = mp.tasks.BaseOptions
-ObjectDetector = mp.tasks.vision.ObjectDetector
-ObjectDetectorOptions = mp.tasks.vision.ObjectDetectorOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
+BaseOptions = python.BaseOptions
+ObjectDetector = vision.ObjectDetector
+ObjectDetectorOptions = vision.ObjectDetectorOptions
+VisionRunningMode = vision.RunningMode
+
+IMAGE_FILE = "https://storage.googleapis.com/mediapipe-tasks/object_detector/cat_and_dog.jpg"
+
+MARGIN = 10  # pixels
+ROW_SIZE = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+TEXT_COLOR = (255, 0, 0)  # red
+
+
+def visualize(
+    image,
+    detection_result
+) -> np.ndarray:
+    """Draws bounding boxes on the input image and return it.
+    Args:
+      image: The input RGB image.
+      detection_result: The list of all "Detection" entities to be visualize.
+    Returns:
+      Image with bounding boxes.
+    """
+    for detection in detection_result.detections:
+        # Draw bounding_box
+        bbox = detection.bounding_box
+        start_point = bbox.origin_x, bbox.origin_y
+        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+        cv2.rectangle(image, start_point, end_point, TEXT_COLOR, 3)
+
+        # Draw label and score
+        category = detection.categories[0]
+        category_name = category.category_name
+        probability = round(category.score, 2)
+        result_text = category_name + ' (' + str(probability) + ')'
+        text_location = (MARGIN + bbox.origin_x,
+                         MARGIN + ROW_SIZE + bbox.origin_y)
+        cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                    FONT_SIZE, TEXT_COLOR, FONT_THICKNESS)
+
+    return image
 
 
 def detect_image() -> None:
@@ -25,4 +67,18 @@ def detect_image() -> None:
     )
 
     with ObjectDetector.create_from_options(options) as detector:
-        pass
+        # Load the input image
+        image = cv2.imread(IMAGE_FILE)
+        cv2.imshow(image)
+
+        # Detect objects in the input image
+        detection_result = detector.detect(image)
+
+        # Process the detection result, in this case, visualize it
+        image_copy = np.copy(image.numpy_view())
+        annotated_image = visualize(image_copy, detection_result)
+        rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+        cv2.imshow(rgb_annotated_image)
+
+if __name__ == '__main__':
+    detect_image()
